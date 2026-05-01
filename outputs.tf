640721835da48c1c7a67c0c45f68de88e4170104
CorpLynx <brichard3132@gmail.com>
2026-05-01 11:34:01 -0400

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "folder_ids" {
-  description = "Map of folder name to fully-qualified folder ID."
-  value = {
-    test          = google_folder.test.id
-    tld_aw_folder = google_folder.tld_aw_folder.id
-  }
-}
+# output "folder_ids" {
+#   description = "Map of folder name to fully-qualified folder ID."
+#   value = {
+#     tld_aw_folder = google_folder.tld_aw_folder.id
+#     frh_folder    = google_assured_workloads_workload
+#   }
+# }
